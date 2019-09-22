96a925d42090207f2e759d92282c309645293d3b
Carlos Ibarra <ntic.carlos@gmail.com>
2019-09-22 11:59:14 -0700

    Adding styles

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <p>Calories: {calories}</p>
-            <img src={image} alt="recipe img"/>
+            <img 
+            className={style.image} 
+            src={image} 
+            alt="recipe img"/>
             <ol>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
